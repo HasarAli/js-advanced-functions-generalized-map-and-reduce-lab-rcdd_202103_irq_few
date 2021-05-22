69e51d7d5f21556166c0bdd7f93e9755046f84be
Hasar Ali <hasar.ali@protonmail.com>
2021-05-22 02:43:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+// WHY DOES THIS NOT PASS THE TESTS????!!!!
+function mapLike(arr, operation = function(x) {return x}) {
+  arrNew = []
+  for (let i = 0; i < arr.length; i++) {
+    arrNew[i] = operation(arr[i])
+  }
+  return arrNew
+}
+// BUT THIS PASSES!!
+
+// function map(arr, operation = function(x) {return x}) {
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] = operation(arr[i])
+//   }
+//   return arr
+// }
+
+
+function reduce(arr, startingPoint, operation = function(x) {return x}) {
+  for (let element of arr) {
+    startingPoint = operation(startingPoint, element)
+  }
+  return startingPoint
+}
