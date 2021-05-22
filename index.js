f0b44537638ca927300caa2808488a99d9416344
Hasar Ali <hasar.ali@protonmail.com>
2021-05-22 02:48:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,37 +40,37 @@ function mapToNegativize(sourceArray) {
   return map(sourceArray, function(x){return x * -1})
 }
 
-function mapToNoChange(sourceArray) {
-  return mapLike(sourceArray)
-}
+// function mapToNoChange(sourceArray) {
+//   return mapLike(sourceArray)
+// }
 
-function mapToDouble(sourceArray) {
-  return mapLike(sourceArray, function(x) {return x*2})
-}
+// function mapToDouble(sourceArray) {
+//   return mapLike(sourceArray, function(x) {return x*2})
+// }
 
-function mapToSquare(sourceArray) {
-  return mapLike([...sourceArray], function(x) {return x*x})
-}
+// function mapToSquare(sourceArray) {
+//   return mapLike([...sourceArray], function(x) {return x*x})
+// }
 
 
-function reduceLike(arr, startingPoint, operation = function(x) {return x}) {
-  for (let i = 0; i < arr.length; i++) {
-    startingPoint = operation(startingPoint, arr[i])
-  }
-  return startingPoint
-}
+// function reduceLike(arr, startingPoint, operation = function(x) {return x}) {
+//   for (let i = 0; i < arr.length; i++) {
+//     startingPoint = operation(startingPoint, arr[i])
+//   }
+//   return startingPoint
+// }
 
-function reduceToTotal(sourceArray, startingPoint = 0) {
-  const total = function(sp, arrElement){return sp + arrElement}
-  return reduceLike(sourceArray, startingPoint, total)
-}
+// function reduceToTotal(sourceArray, startingPoint = 0) {
+//   const total = function(sp, arrElement){return sp + arrElement}
+//   return reduceLike(sourceArray, startingPoint, total)
+// }
 
-function reduceToAllTrue(sourceArray) {
-  const checkAllTrue = function (sp, arrElement) {return sp && !!arrElement}
-  return reduceLike(sourceArray, true, checkAllTrue)
-}
+// function reduceToAllTrue(sourceArray) {
+//   const checkAllTrue = function (sp, arrElement) {return sp && !!arrElement}
+//   return reduceLike(sourceArray, true, checkAllTrue)
+// }
 
-function reduceToAnyTrue(sourceArray) {
-  const checkAllTrue = function (sp, arrElement) {return sp || !!arrElement}
-  return reduceLike(sourceArray, false, checkAllTrue)
-}
+// function reduceToAnyTrue(sourceArray) {
+//   const checkAllTrue = function (sp, arrElement) {return sp || !!arrElement}
+//   return reduceLike(sourceArray, false, checkAllTrue)
+// }
